2f4f3a4aff21d1ce48c074b9d129e64cb88bd081
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-02 11:48:32 +0100

    Exercise 8 completed

--- app.js
+++ app.js
@@ -282,3 +282,18 @@ const isEveryone19OrOlder = devs.every((dev) => {
 // Check your work:
 console.log("Exercise 7 my result: ", isEveryone19OrOlder);
 console.log("Exercise 7 correct result: ", false);
+
+/*
+Exercise 8: Array.prototype.find()
+
+Use Array.prototype.find() to identify and retrieve the comment object with 
+a specific ID 823423 from an array of comment objects.
+
+- Assign the found comment object to the variable 'commentById'.
+*/
+
+const commentById = comments.find((comment) => comment.id === 823423);
+
+// Check your work:
+console.log("Exercise 8 my result: ", commentById);
+console.log("Exercise 8 correct result: ", { text: "Super good", id: 823423 });
