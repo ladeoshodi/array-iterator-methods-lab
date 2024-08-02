8d1c6e5c8010785dee23d72ef66c796fdf45a4a6
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-02 11:25:05 +0100

    Exercise 5 completed

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ from an array of inventor objects
 - Assign the found inventor object to the variable inventorNamedAda
 */
 
-let inventorNamedAda = inventors.find((inventor) => inventor.first === "Ada");
+const inventorNamedAda = inventors.find((inventor) => inventor.first === "Ada");
 
 // Check your work:
 console.log("Exercise 4 my result: ", inventorNamedAda);
@@ -178,3 +178,64 @@ console.log("Exercise 4 correct result: ", {
   year: 1815,
   passed: 1852,
 });
+
+/*
+Exercise 5: Array.prototype.map()
+
+Use the Array.prototype.map() method to reformat each name in the 'people' 
+array. The goal is to convert names from "Last, First" format to "First Last" 
+format.
+
+Hint: Use the String.prototype.split() method to separate the first and last 
+      names. You can split the string using ', ' as the separator.
+      After splitting the names, rearrange them to the "First Last" format.
+*/
+
+const firstLast = people.map((person) => {
+  return person.split(", ").reverse().join(" ");
+});
+
+// Check your work:
+console.log("Exercise 5 my result: ", firstLast);
+console.log("Exercise 5 correct result: ", [
+  "Carl Becker",
+  "Samuel Beckett",
+  "Mick Beddoes",
+  "Henry Beecher",
+  "Ludwig Beethoven",
+  "Menachem Begin",
+  "Hilaire Belloc",
+  "Saul Bellow",
+  "Robert Benchley",
+  "Peter Benenson",
+  "David Ben-Gurion",
+  "Walter Benjamin",
+  "Tony Benn",
+  "Chester Bennington",
+  "Leana Benson",
+  "Silas Bent",
+  "Lloyd Bentsen",
+  "Ric Berger",
+  "Ingmar Bergman",
+  "Luciano Berio",
+  "Milton Berle",
+  "Irving Berlin",
+  "Eric Berne",
+  "Sandra Bernhard",
+  "Yogi Berra",
+  "Halle Berry",
+  "Wendell Berry",
+  "Erin Bethea",
+  "Aneurin Bevan",
+  "Ken Bevel",
+  "Joseph Biden",
+  "Ambrose Bierce",
+  "Steve Biko",
+  "Josh Billings",
+  "Frank Biondo",
+  "Augustine Birrell",
+  "Elk Black",
+  "Robert Blair",
+  "Tony Blair",
+  "William Blake",
+]);
